7b2cbabdab2153e7ed23a2d8a6110d87a08e4114
Ryan Uber <ru@ryanuber.com>
2017-02-14 23:13:01 -0800

    Cleanup and code comments

--- duration.rb
+++ duration.rb
@@ -1,73 +1,118 @@
-class GoDuration
+module GoTime
+  class Duration
+    # One nanosecond
+    NANOSECOND=1.freeze
 
-  NANOSECOND=1.freeze
-  MICROSECOND=1000*NANOSECOND.freeze
-  MILLISECOND=1000*MICROSECOND.freeze
-  SECOND=1000*MILLISECOND.freeze
-  MINUTE=60*SECOND.freeze
-  HOUR=60*MINUTE.freeze
+    # One microsecond
+    MICROSECOND=1000*NANOSECOND.freeze
 
-  UNITS={
-    'h' => HOUR,
-    'm' => MINUTE,
-    's' => SECOND,
-    'ms' => MILLISECOND,
-    'µs' => MICROSECOND,
-    'us' => MICROSECOND,
-    'ns' => NANOSECOND,
-  }.freeze
+    # One millisecond
+    MILLISECOND=1000*MICROSECOND.freeze
 
-  attr_reader :nanoseconds
+    # One second
+    SECOND=1000*MILLISECOND.freeze
 
-  def initialize(v)
-    @nanoseconds = parse(v)
-  end
+    # One minute
+    MINUTE=60*SECOND.freeze
 
-  def microseconds
-    nanoseconds / MICROSECOND
-  end
+    # One hour
+    HOUR=60*MINUTE.freeze
 
-  def milliseconds
-    nanoseconds / MILLISECOND
-  end
+    # A mapping of Go's unit notations to their nanosecond values.
+    UNITS={
+      "h" => HOUR,
+      "m" => MINUTE,
+      "s" => SECOND,
+      "ms" => MILLISECOND,
+      "µs" => MICROSECOND,
+      "us" => MICROSECOND,
+      "ns" => NANOSECOND,
+    }.freeze
 
-  def seconds
-    nanoseconds / SECOND
-  end
+    attr_reader :nanoseconds
 
-  def minutes
-    nanoseconds / MINUTE
-  end
-
-  def hours
-    nanoseconds / HOUR
-  end
-
-  def format
-    ns = nanoseconds
-    fmt = ""
-
-    UNITS.each do |unit, value|
-      number, ns = ns.divmod(value)
-      fmt << "#{number}#{unit}" if number > 0
+    # Initializes a new duration instance.
+    #
+    # @param ns [Fixnum] Duration in nanoseconds.
+    #
+    # @return [Duration]
+    def initialize(ns)
+      @nanoseconds = ns
     end
 
-    fmt
-  end
+    # Parses a Go time.Duration string.
+    #
+    # @param duration [String] A Go duration string.
+    #
+    # @return [Duration]
+    def self.parse(duration)
+      ns = 0
+      until duration.empty?
+        number = duration.slice!(/^[[:digit:]]+/).to_i
+        unit = duration.slice!(/^[[:alpha:]]+/)
 
-  private
+        raise "Invalid unit '#{unit}'" unless UNITS[unit]
+        ns += (number * UNITS[unit])
+      end
 
-  def parse(v)
-    ns = 0
-
-    while v.length > 0
-      number = v.slice!(/[[:digit:]]+/).to_i
-      unit = v.slice!(/[[:alpha:]]+/)
-
-      raise "Invalid unit '#{unit}'" unless UNITS[unit]
-      ns += (number * UNITS[unit])
+      self.new(ns)
     end
 
-    ns
+    # Returns the number of microseconds in the duration.
+    #
+    # @return [Fixnum]
+    def microseconds
+      nanoseconds / MICROSECOND
+    end
+
+    # Returns the number of milliseconds in the duration.
+    #
+    # @return [Fixnum]
+    def milliseconds
+      nanoseconds / MILLISECOND
+    end
+
+    # Returns the number of seconds in the duration.
+    #
+    # @return [Fixnum]
+    def seconds
+      nanoseconds / SECOND
+    end
+
+    # Returns the number of minutes in the duration.
+    #
+    # @return [Fixnum]
+    def minutes
+      nanoseconds / MINUTE
+    end
+
+    # Returns the number of hours in the duration.
+    #
+    # @return [Fixnum]
+    def hours
+      nanoseconds / HOUR
+    end
+
+    # Returns the exact duration in nanoseconds.
+    #
+    # @return [Fixnum]
+    def to_i
+      nanoseconds
+    end
+
+    # Formats the duration into a Go duration string.
+    #
+    # @return [String]
+    def to_s
+      ns = nanoseconds
+      fmt = ""
+
+      UNITS.each do |unit, value|
+        number, ns = ns.divmod(value)
+        fmt << "#{number}#{unit}" if number > 0
+      end
+
+      fmt
+    end
   end
 end
